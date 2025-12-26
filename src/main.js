fa0971929879c681f33cd6bf6c3e1feebf36db76
Manishbhai9350 <keshraram294@gmail.com>
2025-12-26 21:04:55 +0530

    DOM Added
    Model Animation Pending

--- src/main.js
+++ src/main.js
@@ -15,10 +15,16 @@ import { Mesh } from "three";
 import { PlaneGeometry } from "three";
 import { MeshBasicMaterial } from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
+import gsap from "gsap";
 
 const { PI } = Math;
 
+const MouseElm = document.querySelector("main .mouse");
+const MouseParaElm = document.querySelector("main .mouse p");
 const canvas = document.querySelector("canvas");
+
+const domMouse = { x: 0, y: 0 };
+const targetDomMouse = { x: 0, y: 0 };
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
@@ -39,7 +45,6 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.z = 5;
 
-
 // Scene Bounds ??
 const { width: SceneWidth, height: SceneHeight } = GetSceneBounds(
   renderer,
@@ -48,8 +53,7 @@ const { width: SceneWidth, height: SceneHeight } = GetSceneBounds(
 
 // Orbit Controls
 
-const controls = new OrbitControls(camera,canvas)
-
+const controls = new OrbitControls(camera, canvas);
 
 // Loaders & Managers ??
 const Manager = new THREE.LoadingManager();
@@ -64,6 +68,7 @@ GLB.setDRACOLoader(Draco);
 // GUI;
 const gui = new GUI();
 
+gui.close();
 
 // GUI Setting
 const settings = {
@@ -71,19 +76,19 @@ const settings = {
     value: 1,
   },
   lerpAlpha: {
-    value: .05,
+    value: 0.05,
   },
   rotationLerpAlpha: {
-    value: .05,
+    value: 0.05,
   },
   radius: {
     value: 3,
   },
-  rx:{
-    value:.07
+  rx: {
+    value: 0.07,
   },
-  ry:{
-    value:.15
+  ry: {
+    value: 0.15,
   },
 };
 
@@ -96,7 +101,7 @@ gui
 gui
   .add(settings.lerpAlpha, "value")
   .min(0)
-  .max(.5)
+  .max(0.5)
   .step(0.0001)
   .name("Lerping Alpha");
 gui
@@ -108,22 +113,11 @@ gui
 gui
   .add(settings.rotationLerpAlpha, "value")
   .min(0)
-  .max(.6)
+  .max(0.6)
   .step(0.001)
   .name("Rotation Lerp Alpha");
-gui
-  .add(settings.rx, "value")
-  .min(0)
-  .max(.2)
-  .step(0.001)
-  .name("Rotation X");
-gui
-  .add(settings.ry, "value")
-  .min(0)
-  .max(.2)
-  .step(0.001)
-  .name("Rotation Y");
-
+gui.add(settings.rx, "value").min(0).max(0.2).step(0.001).name("Rotation X");
+gui.add(settings.ry, "value").min(0).max(0.2).step(0.001).name("Rotation Y");
 
 // Raycaster Mouse
 const mouse = new Vector2(0, 0);
@@ -132,7 +126,9 @@ const mouse3d = new Vector3(0, 0, 0);
 
 // Stone
 let stone = {
+  isStoneActive: true,
   model: null,
+  collider: null,
   mixer: null,
   animations: {
     intro: null,
@@ -141,21 +137,67 @@ let stone = {
   intro: null,
   outro: null,
   loaded: false,
+  currentPlaying: "outro",
+  toPlay:'intro',
+  isMouse: false,
+  playIntro: (stone = {}) => {
+    // play once
+    stone.toPlay = 'intro'
+    stone.isStoneActive = true;
+    stone.intro.reset();
+    stone.intro.setLoop(THREE.LoopOnce, 1);
+    stone.intro.clampWhenFinished = true;
+    stone.intro.play();
+  },
+  playOutro: (stone = {}) => {
+    // play once
+    stone.toPlay = 'outro'
+    stone.isStoneActive = false;
+
+    // Only Play When model is ready ? Ready means the stones piece are all at the starting location
+    // Same for the Intro
+
+
+    // stone.outro.reset();
+    // stone.outro.setLoop(THREE.LoopOnce, 1);
+    // stone.outro.clampWhenFinished = true;
+    // stone.outro.play();
+  },
   positions: {},
 };
 GLB.load("/stone.glb", (glb) => {
   stone.model = glb.scene;
+  stone.collider = stone.model.clone(true);
   stone.animations.intro = glb.animations[0];
   stone.animations.outro = glb.animations[1];
   stone.mixer = new THREE.AnimationMixer(stone.model);
   stone.intro = stone.mixer.clipAction(stone.animations.intro);
   stone.outro = stone.mixer.clipAction(stone.animations.outro);
+
+  stone.mixer.addEventListener("finished", (e) => {
+    if (e.action === stone.intro || e.action === stone.outro) {
+      console.log("Something Finished");
+      stone.currentPlaying = "none";
+      stone.toPlay = 'none'
+    }
+  });
+
   stone.loaded = true;
 
-  stone.intro.play();
+  stone.playIntro(stone);
+
+  const colliderMaterial = new MeshBasicMaterial({
+    color: 0xff0000,
+    visible: false,
+  });
+
+  stone.collider.traverse((mesh) => {
+    if (mesh.isMesh) {
+      mesh.material = colliderMaterial;
+    }
+  });
 
   // Copying all the positions
-
   stone.model.traverse((node) => {
     if (!node.isMesh) return;
     // world position of node
@@ -164,25 +206,24 @@ GLB.load("/stone.glb", (glb) => {
     stone.positions[node.userData.name] = worldPos;
   });
 
-  scene.add(stone.model);
+  scene.add(stone.model, stone.collider);
   stone.model.position.set(-0.25, 0, 0);
   stone.model.scale.setScalar(0.9);
   stone.model.rotation.set(Math.PI / 10, 0, 0);
+  stone.collider.position.copy(stone.model.position);
+  stone.collider.scale.setScalar(0.9);
+  stone.collider.rotation.set(Math.PI / 10, 0, 0);
 });
-
 
 // Raycaster Plane;
 
 const RayPlane = new Mesh(
-  new PlaneGeometry(SceneWidth,SceneHeight),
-  new MeshBasicMaterial({color:0xff0000,transparent:true,visible:false})
-)
+  new PlaneGeometry(SceneWidth, SceneHeight),
+  new MeshBasicMaterial({ color: 0xff0000, transparent: true, visible: false })
+);
 
 RayPlane.rotateX.x = Math.PI / 2;
-scene.add(RayPlane)
-
-
-
+scene.add(RayPlane);
 
 // ?? Lights
 const Amb = new AmbientLight(0xffffff, 0.5);
@@ -194,32 +235,22 @@ Top.position.set(0, 4, 0);
 
 scene.add(Amb, Dir, Top);
 
-
-
-
 // Raycaster ??
 const raycaster = new Raycaster();
 
-
-
-// Render Pipeline ??
-const clock = new Clock();
-let PrevTime = clock.getElapsedTime();
-
 // Displacement Position Vector
-const displacedPos = new Vector3()
-const rotation = new Vector2()
+const displacedPos = new Vector3();
+const rotation = new Vector2();
 
-function Animate() {
-  const CurrentTime = clock.getElapsedTime();
-  const DT = CurrentTime - PrevTime;
-  PrevTime = CurrentTime;
+function updateAnimations(DT = 0){
+   if (stone.currentPlaying !== "none") {
+      stone.mixer.update(DT);
+    }
+}
 
-  mouse.x += (targetMouse.x - mouse.x) * 0.15;
-  mouse.y += (targetMouse.y - mouse.y) * 0.15;
-
+// Animating The Stone
+function AnimateStone(DT = 0) {
   if (stone.loaded) {
-    // stone.mixer.update(DT)
 
     stone.model.traverse((node) => {
       if (!node.isMesh) return;
@@ -243,27 +274,47 @@ function Animate() {
         (1 - dist / settings.radius.value) * settings.displaceStrength.value;
 
       // Displaced Position
-      displacedPos.copy(worldPos)
-        .add(dir.multiplyScalar(strength));
-
+      displacedPos.copy(worldPos).add(dir.multiplyScalar(strength));
 
       // apply displacement
       node.position.lerp(displacedPos, settings.lerpAlpha.value);
     });
 
+    stone.model.rotation.x +=
+      (rotation.x - stone.model.rotation.x) * settings.rotationLerpAlpha.value;
+    stone.model.rotation.y +=
+      (rotation.y - stone.model.rotation.y) * settings.rotationLerpAlpha.value;
 
-    stone.model.rotation.x += (rotation.x - stone.model.rotation.x)  * settings.rotationLerpAlpha.value
-    stone.model.rotation.y += (rotation.y - stone.model.rotation.y)  * settings.rotationLerpAlpha.value
-
+    stone.collider.rotation.x = stone.model.rotation.x;
+    stone.collider.rotation.y = stone.model.rotation.y;
   }
+}
+
+// Render Pipeline ??
+const clock = new Clock();
+let PrevTime = clock.getElapsedTime();
+
+function Animate() {
+  const CurrentTime = clock.getElapsedTime();
+  const DT = CurrentTime - PrevTime;
+  PrevTime = CurrentTime;
+
+  mouse.x += (targetMouse.x - mouse.x) * 0.15;
+  mouse.y += (targetMouse.y - mouse.y) * 0.15;
+
+  domMouse.x += (targetDomMouse.x - domMouse.x) * 0.05;
+  domMouse.y += (targetDomMouse.y - domMouse.y) * 0.05;
+
+  MouseElm.style.left = `${domMouse.x}px`;
+  MouseElm.style.top = `${domMouse.y}px`;
+
+  AnimateStone(DT);
 
   renderer.render(scene, camera);
   requestAnimationFrame(Animate);
 }
 
 requestAnimationFrame(Animate);
-
-
 
 // Resization ??
 function resize() {
@@ -274,30 +325,62 @@ function resize() {
   renderer.setSize(innerWidth, innerHeight);
 }
 
-
 // Mouse Move ??
 function mouseMove(e) {
   if (!stone.loaded) return;
   const x = e.clientX;
   const y = e.clientY;
 
+  targetDomMouse.x = x;
+  targetDomMouse.y = y;
+
   const nx = (x / innerWidth) * 2 - 1;
   const ny = -((y / innerHeight) * 2 - 1);
 
   mouse.set(nx, ny);
-  rotation.set(
-    .3 + settings.rx.value * -ny, 
-    settings.ry.value * nx, 
-  )
+  rotation.set(0.3 + settings.rx.value * -ny, settings.ry.value * nx);
 
   raycaster.setFromCamera(mouse, camera);
-  const intersects = raycaster.intersectObject(RayPlane);
+  const intersects = raycaster.intersectObject(stone.collider, true);
 
   if (intersects && intersects.length) {
     const intersect = intersects[0];
     mouse3d.copy(intersect.point);
+    stone.isMouse = true;
+    gsap.to(MouseElm, {
+      color: "white",
+      borderColor: "white",
+    });
+    gsap.to(MouseParaElm,{
+      opacity:1
+    })
+    document.body.style.cursor = "pointer";
+  } else {
+    gsap.to(MouseElm, {
+      color: "black",
+      borderColor: "black",
+    });
+    gsap.to(MouseParaElm,{
+      opacity:0
+    })
+    stone.isMouse = false;
+    document.body.style.cursor = "initial";
   }
 }
 
-window.addEventListener("mousemove", mouseMove);
+function onClick() {
+  if (!stone.isMouse || !stone.loaded || stone.currentPlaying !== "none")
+    return;
+
+  if (stone.isStoneActive) {
+    stone.playOutro(stone);
+    stone.isStoneActive = false;
+  } else {
+    stone.playIntro(stone);
+    stone.isStoneActive = true;
+  }
+}
+
 window.addEventListener("resize", resize);
+window.addEventListener("mousemove", mouseMove);
+window.addEventListener("click", onClick);
